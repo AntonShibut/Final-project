631581e2e995b3d10988af46d1e0ea04d61b84cf
Anton <messenger5251@gmail.com>
2020-09-10 21:17:21 +0300

    10/09 class

---
+++ src/AppHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function AppHeader() {
+    return (
+            <header className="App-header">
+                <img className="App-logo" alt="logo" />
+                <div className="searche-box">
+                </div>
+            </header>
+    );
+}
+
---
+++ src/AppSearche.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default AppSearche() {
+    return (
+        <div></div>
+    )
+}
